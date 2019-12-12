4f591c70871cfb5b1274e5c13fa9be11e4d628cc
wenhaian <787766452@qq.com>
2019-12-12 13:11:04 +0800

    修正了vuex存在的问题

--- gym-system/src/store/index.js
+++ gym-system/src/store/index.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user'
 import goods from './modules/goods'
+import coach from './modules/coach'
+import order from './modules/order'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules:{
     user,
-    goods
+    goods,
+    coach,
+    order
   }
 })
